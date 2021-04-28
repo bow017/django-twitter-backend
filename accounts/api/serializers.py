43e35ae761284e904a746e70fe2dc7a5c93ff710
bow017 <bow017@ucsd.edu>
2021-04-28 13:05:57 +0800

    add user login, logout, login status check, user signup

--- accounts/api/serializers.py
+++ accounts/api/serializers.py
@@ -1,8 +1,47 @@
-from django.contrib.auth.models import User, Group
-from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework import serializers, exceptions
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ('id', 'username', 'email')
+        fields = ('username', 'email')
+
+
+class SignupSerializer(serializers.ModelSerializer):
+    username = serializers.CharField(max_length=20, min_length=6)
+    password = serializers.CharField(max_length=20, min_length=6)
+    email = serializers.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'email')
+
+    # is_valid()
+    def validate(self, data):
+        if User.objects.filter(username=data['username'].lower()).exists():
+            raise exceptions.ValidationError({
+                'username': 'Username already exists.'
+            })
+        if User.objects.filter(email=data['email'].lower()).exists():
+            raise exceptions.ValidationError({
+                'email': 'Email already exists.'
+            })
+        return data
+
+    # .save()
+    def create(self, validated_data):
+        return User.objects.create_user(
+            username=validated_data['username'].lower(),
+            email=validated_data['email'].lower(),
+            password=validated_data['password'],
+        )
+
+
+class LoginSerializer(serializers.ModelSerializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'password')
