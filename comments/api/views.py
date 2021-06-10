039c50fab12a6391dd37741271bd1b9de89a795a
bow017 <bow017@ucsd.edu>
2021-06-10 22:02:08 +0800

    adding comment update and destroy functionalities with unit tests

--- comments/api/views.py
+++ comments/api/views.py
@@ -5,11 +5,12 @@ from comments.models import Comment
 from comments.api.serializers import (
     CommentSerializer,
     CommentSerializerForCreate,
+    CommentSerializerForUpdate,
 )
+from comments.api.permissions import IsObjectOwner
 
 
 class CommentViewSet(viewsets.GenericViewSet):
-
     # Implementation of list, create, update, destroy
     # Not implementing retrieve as it is not needed
 
@@ -19,6 +20,8 @@ class CommentViewSet(viewsets.GenericViewSet):
     def get_permissions(self):
         if self.action == 'create':
             return [IsAuthenticated()]
+        if self.action in ['destroy', 'update']:
+            return [IsAuthenticated(), IsObjectOwner()]
         return [AllowAny()]
 
     def create(self, request, *args, **kwargs):
@@ -40,3 +43,28 @@ class CommentViewSet(viewsets.GenericViewSet):
             CommentSerializer(comment).data,
             status=status.HTTP_201_CREATED,
         )
+
+    def update(self, request, *args, **kwargs):
+        # get_object will raise 404 error if not found
+        serializer = CommentSerializerForUpdate(
+            instance=self.get_object(),
+            data=request.data,
+        )
+        if not serializer.is_valid():
+            raise Response({
+                'message': 'Please check input'
+            }, status=status.HTTP_400_BAD_REQUEST)
+
+        comment = serializer.save()
+
+        return Response(
+            CommentSerializer(comment).data,
+            status=status.HTTP_200_OK,
+        )
+
+    def destroy(self, request, *args, **kwargs):
+        comment = self.get_object()
+        comment.delete()
+
+        # DRF default destroy status code is 204 no content
+        return Response({'success': True}, status=status.HTTP_200_OK)
